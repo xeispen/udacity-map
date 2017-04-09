c9b32a66bfbba8b9b010bec2399b435843c695e6
Peisen Xue <peisen.xue@gmail.com>
2017-04-09 11:45:42 -0700

    revised

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 // Global map variable
 var map;
-
+var placeInfoWindow;
 // Global placemarkers array to use in multiple functions to have control
 // over the number of places that show
 var placeMarkers = [];
-
+var defaultIcon;
+var highlightedIcon;
 // YELP API
 var YELP_KEY = "OxZvVRYUynQrXWGlc5kGvQ";
 var YELP_KEY_SECRET = "arYjXn2ei0ck3TIjAaNdoGlWDKQ";
@@ -26,6 +27,13 @@ function resize() {
     map.panTo(center);
 }
 
+function populate() {
+    document.getElementById("zoom-to-area").click();
+    setTimeout(function(){document.getElementById("go-places").click()},1000);
+}
+
+
+
 var stringStartsWith = function(string, startsWith) {          
     string = string || "";
     if (startsWith.length > string.length)
@@ -33,11 +41,11 @@ var stringStartsWith = function(string, startsWith) {
     return string.substring(0, startsWith.length) === startsWith;
 };
 
+function mapError() {
+    alert("Could not load Google Maps!!!");
+}
 
-
-// MOVE: into viewModel
 function initMap() {
-	console.log('initMap');
 	// Create a map object and display it in the div of id="map"
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat:37.09024, lng:-95.712891},
@@ -47,7 +55,13 @@ function initMap() {
 		styles: styles,
 		zoom: 4
 	});
-
+    // Create a single infowindow to be used with the place details information 
+    // so that only one is open at once
+    placeInfoWindow = new google.maps.InfoWindow();
+    // Styled markers for each location
+    defaultIcon = makeMarkerIcon('0091ff');
+    // Highlighted marker for when users mouseover
+    highlightedIcon = makeMarkerIcon('FFFF24');
     // This autocomplete is for use in the geocoder entry box
     var zoomAutocomplete = new google.maps.places.Autocomplete(
         document.getElementById('zoom-to-area-text'));
@@ -67,11 +81,11 @@ function initMap() {
 // locates it, and then zooms into that area. This is so that the user can
 // show all of the listings and decide to focus on a single area of the map
 
-function zoomToArea() {
+function zoomToArea(destination) {
     // Initialize the geocoder
     var geocoder = new google.maps.Geocoder();
     // Gets the destination that the user entered and sets it to the global variable
-    var destination = document.getElementById('zoom-to-area-text').value;
+    // var destination = document.getElementById('zoom-to-area-text').value;
     // Make sure input is not blank
     if (destination == '') {
         window.alert('You must enter a destination!');
@@ -105,6 +119,12 @@ function makeInvisible() {
 	for (var i = 0; i < placeMarkers.length; i++) {
 		placeMarkers[i].setVisible(false);
 	}
+}
+
+function makeDefault() {
+    for (var i = 0; i < placeMarkers.length; i++) {
+        placeMarkers[i].setIcon(defaultIcon);
+    }
 }
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
@@ -157,124 +177,12 @@ function makeMarkerIcon(markerColor) {
 }
 
 
-
-// // This function fires when the user selects "go" on the places search
-// // It will do a nearby search using the entered query string or place
-// function textSearchPlaces() {
-// 	var deferred = $.Deferred();
-//     var bounds = map.getBounds();
-//     hideMarkers(placeMarkers);
-//     var placesService = new google.maps.places.PlacesService(map);
-//     placesService.textSearch({
-//         query: document.getElementById('places-search').value,
-//         bounds: bounds
-//     }, function(results, status) {
-//         if (status === google.maps.places.PlacesServiceStatus.OK) {
-//         	deferred.resolve(results);
-//             //createMarkersForPlaces(results);
-//         }
-//     });
-//     return deferred.promise();
-// }
-
-// // This function creates markers for each place found in either places search
-// function createMarkersForPlaces(places) {
-// 	deleteMarkers();
-// 	console.log(places);
-//     var bounds = new google.maps.LatLngBounds();
-//     for (var i = 0; i < places.length; i++) {
-//         var place = places[i];
-//         var icon = {
-//             url: place.icon,
-//             size: new google.maps.Size(35, 35),
-//             origin: new google.maps.Point(0, 0),
-//             anchor: new google.maps.Point(15, 34),
-//             scaledSize: new google.maps.Size(25, 25)
-//         };
-//         // Create a marker for each place
-//         var marker = new google.maps.Marker({
-//             map: map,
-//             icon: icon,
-//             title: place.name,
-//             position: place.geometry.location,
-//             id: place.place_id
-//         });
-//         // Create a single infowindow to be used with the place details information 
-//         // so that only one is open at once
-//         var placeInfoWindow = new google.maps.InfoWindow();
-//         // If a marker is clicked, do a place details search on it in the next function
-//         marker.addListener('click', function() {
-//             if (placeInfoWindow.marker == this) {
-//                 console.log("This infowindow is already on this marker@!");
-//             } else {
-//                 // `this` is the marker
-//                 console.log('opening infowindow');
-//                 getPlacesDetails(this, placeInfoWindow);
-//             }
-//         });
-//         placeMarkers.push(marker);
-//         if (place.geometry.viewport) {
-//             // Only geocodes have viewport
-//             bounds.union(place.geometry.viewport);
-//         } else {
-//             bounds.extend(place.geometry.location);
-//         }
-//     }
-//     map.fitBounds(bounds);
-// }
-
-
-
-// // This is the PLACES DETAILS search - it's the most detailed so it is only
-// // executed when a marker is selected, indicating the user wants more details about place
-// function getPlacesDetails(marker, infowindow) {
-//     var service = new google.maps.places.PlacesService(map);
-//     service.getDetails({
-//         placeId: marker.id
-//     }, function(place, status) {
-//         if (status === google.maps.places.PlacesServiceStatus.OK) {
-//             // Set the marker property on this infowindow so it isn't created again
-//             infowindow.marker = marker;
-//             var innerHTML = '<div>';
-//             if (place.name) {
-//                 innerHTML += '<strong>' + place.name + '</strong>';
-//             }
-//             if (place.formatted_address) {
-//                 innerHTML += '<br>' + place.formatted_address;
-//             }
-//             if (place.formatted_phone_number) {
-//                 innerHTML += '<br>' + place.formatted_phone_number;
-//             }
-//             if (place.opening_hours) {
-//                 innerHTML += '<br><br><strong>Hours:</strong><br>' +
-//                     place.opening_hours.weekday_text[0] + '<br>' +
-//                     place.opening_hours.weekday_text[1] + '<br>' +
-//                     place.opening_hours.weekday_text[2] + '<br>' +
-//                     place.opening_hours.weekday_text[3] + '<br>' +
-//                     place.opening_hours.weekday_text[4] + '<br>' +
-//                     place.opening_hours.weekday_text[5] + '<br>' +
-//                     place.opening_hours.weekday_text[6];
-//             }
-//             if (place.photos) {
-//                 innerHTML += '<br><br><img src="' + place.photos[0].getUrl({ maxHeight: 100, maxWidth: 200 }) + '">';
-//             }
-//             innerHTML += '</div>';
-//             infowindow.setContent(innerHTML);
-//             infowindow.open(map, marker);
-//             // Make sure the marker property is cleared if the infowindow is closed
-//             infowindow.addListener('closeclick', function() {
-//                 infowindow.marker = null;
-//             });
-//         }
-//     });
-// }
-
 // Generate the oauth token to use
 function nonce_generate() {
     return (Math.floor(Math.random() * 1e12).toString());
 }
 
-function getYelp() {
+function getYelp(place) {
 	var deferred = $.Deferred();
 	var bounds = map.getBounds();
 	var ne_latlng = bounds.getNorthEast();
@@ -291,7 +199,7 @@ function getYelp() {
     	oauth_nonce: nonce_generate(),
     	oauth_version: '1.0',
     	callback: 'cb',
-    	term: document.getElementById('places-search').value,
+    	term: place,
 		bounds: sw_latlng.lat() + ',' + sw_latlng.lng() +
 			'|' + ne_latlng.lat() + ',' + ne_latlng.lng()
     }
@@ -308,8 +216,8 @@ function getYelp() {
 		success: function(response) {
 	        deferred.resolve(response.businesses);
 		},
-		fail: function() {
-			console.log('fail');
+		error: function() {
+			document.getElementById('error').innerHTML = "There was an issue retrieving results";
 		}
 	};
 
@@ -318,43 +226,6 @@ function getYelp() {
 	return deferred.promise();
 }
 
-// // This function creates markers for each place found in either places search
-// function createMarkersForYelp(places) {
-// 	deleteMarkers();
-//     var bounds = new google.maps.LatLngBounds();
-//     for (var i = 0; i < places.length; i++) {
-//         var place = places[i];
-
-//         console.log(place);
-//         // Create a new google latlng for marker
-//         var latlng = new google.maps.LatLng(place.location.coordinate.latitude, 
-//         	place.location.coordinate.longitude);
-//         // Create a marker for each place
-//         var marker = new google.maps.Marker({
-//             map: map,
-//             title: place.name,
-//             position: latlng,
-//             id: place.id
-//         });
-//         // Create a single infowindow to be used with the place details information 
-//         // so that only one is open at once
-//         var placeInfoWindow = new google.maps.InfoWindow();
-//         // If a marker is clicked, do a place details search on it in the next function
-//         marker.addListener('click', function() {
-//             if (placeInfoWindow.marker == this) {
-//                 console.log("This infowindow is already on this marker!");
-//             } else {
-//                 // `this` is the marker
-//                 console.log('opening infowindow');
-//                 getYelpDetails(this, placeInfoWindow);
-//             }
-//         });
-//         placeMarkers.push(marker);
-// 		bounds.extend(latlng);    
-// 	}
-//     map.fitBounds(bounds);
-// }
-
 
 // This function creates markers for each place found in either places search
 function createMarker(place) {
@@ -362,11 +233,6 @@ function createMarker(place) {
     var latlng = new google.maps.LatLng(place.location.coordinate.latitude, 
     	place.location.coordinate.longitude);
     
-	// Styled markers for each location
-	var defaultIcon = makeMarkerIcon('0091ff');
-
-	// Highlighted marker for when users mouseover
-	var highlightedIcon = makeMarkerIcon('FFFF24');
 
     var marker = new google.maps.Marker({
         map: map,
@@ -376,9 +242,7 @@ function createMarker(place) {
         animation: google.maps.Animation.DROP,
         id: place.id
     });
-    // Create a single infowindow to be used with the place details information 
-    // so that only one is open at once
-    var placeInfoWindow = new google.maps.InfoWindow();
+
     // If a marker is clicked, do a place details search on it in the next function
     marker.addListener('click', function() {
         if (placeInfoWindow.marker == this) {
@@ -396,11 +260,7 @@ function createMarker(place) {
 // This is the YELP DETAILS search - it's the most detailed so it is only
 // executed when a marker is selected, indicating the user wants more details about place
 function getYelpDetails(marker, infowindow) {
-	// Styled markers for each location
-	var defaultIcon = makeMarkerIcon('0091ff');
-	// Highlighted marker for when users mouseover
-	var highlightedIcon = makeMarkerIcon('FFFF24');
-
+    makeDefault();
 	marker.setIcon(highlightedIcon);
 
 	var YELP_URL = "https://api.yelp.com/v2/business/" + marker.id;
@@ -456,8 +316,11 @@ function getYelpDetails(marker, infowindow) {
                 infowindow.marker = null;
             });
 		},
-		fail: function() {
-			console.log('fail');
+		error: function() {
+            console.log("error");
+            infowindow.marker = marker;
+			infowindow.setContent("<div>Could not retrieve Yelp data!</div>");
+            infowindow.open(map,marker);
 		}
 	};
 
@@ -471,20 +334,18 @@ function getYelpDetails(marker, infowindow) {
 var Place = function(data) {
 	this.name = ko.observable(data.title);
 	
-	var placeInfoWindow = new google.maps.InfoWindow();
 	this.infoWindow = function() {
 		infoWindow(this.name(), placeInfoWindow);
 	}
 }
 
 var ViewModel = function() {
-	console.log('ViewModel');
 	// self always maps to this ViewModel
 	var self = this;
 	// Stores name of destination user entered
-	this.currentDestination = ko.observable();
+	this.currentDestination = ko.observable('Santa Barbara, CA');
 	// Stores the keyword of the current places search
-	this.currentPlace = ko.observable();
+	this.currentPlace = ko.observable('coffee');
 	// current list of places returned by textSearchPlaces
 	this.placesList = ko.observableArray([]);
 	// Current filter value
@@ -496,16 +357,18 @@ var ViewModel = function() {
 			return self.placesList();
 		} else {
 			return ko.utils.arrayFilter(self.placesList(), function(item) {
-				return stringStartsWith(item.name().toLowerCase(), filter);
+				if (item.name().toLowerCase().indexOf(filter) > -1){
+                    return item.name();
+                }
 			});
 		}
 	}, this); 
 
 	// Updates the currentDestination when user enters a new destination
 	this.changeDestination = function() {
-		var newDestination = document.getElementById('zoom-to-area-text').value;
-		self.currentDestination(newDestination);
-		zoomToArea();
+		// var newDestination = document.getElementById('zoom-to-area-text').value;
+		// self.currentDestination(newDestination);
+		zoomToArea(this.currentDestination());
 	};
 	// Updates the currentKeyword when user enters a new interest
 	this.changePlaces = function() {
@@ -513,19 +376,21 @@ var ViewModel = function() {
 		if (self.currentDestination() == null) {
 			window.alert("Please enter a destination for specific results");
 		};
-		var newPlaces = document.getElementById('go-places').value;
 		// Sets a deferred within textSearchPlaces and waits until that is done
-		self.currentPlace(newPlaces);
+		self.currentPlace(this.currentPlace());
 
-		$.when(getYelp()).done(function(data){
+		$.when(getYelp(this.currentPlace())).done(function(data){
 			self.placesList.removeAll();
 			deleteMarkers();
 
     		var bounds = new google.maps.LatLngBounds();
 
+            google.maps.event.addDomListener(window, 'resize', function() {
+                map.fitBounds(bounds); // `bounds` is a `LatLngBounds` object
+            });
+
 			data.forEach(function(item){
 				var marker = createMarker(item);
-				console.log(marker);
 				self.placesList.push(new Place(marker));
 				bounds.extend(marker.position);
 			});
@@ -545,6 +410,4 @@ var ViewModel = function() {
 
 
 ko.applyBindings(new ViewModel());
-
-
-
+window.onload = populate;
